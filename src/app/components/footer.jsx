f18ed5efe2566e3f60681882c5e30136892298d1
Script-Java <atrindev@gmail.com>
2025-06-18 11:03:42 -0500

    added

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -23,13 +23,25 @@ const Footer = () => {
                 Auto Glass
               </Link>
             </li>
+
             <li>
-              <Link href="/residential" className="hover:underline hover:text-primary">
+              <Link
+                href="/residential"
+                className="hover:underline hover:text-primary"
+              >
                 Residential Glass
               </Link>
             </li>
             <li>
-              <Link href="/quote" className="hover:underline hover:text-primary">
+              <Link href="/blog" className="hover:underline hover:text-primary">
+                Tips & Tricks
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/quote"
+                className="hover:underline hover:text-primary"
+              >
                 Online Quote
               </Link>
             </li>
@@ -45,13 +57,19 @@ const Footer = () => {
           <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-center gap-4 text-sm">
             <span className="flex items-center gap-2 justify-center">
               <FaPhone className="shrink-0" />
-              <Link href="tel:9729007194" className="hover:underline hover:text-primary">
+              <Link
+                href="tel:9729007194"
+                className="hover:underline hover:text-primary"
+              >
                 972-900-7194
               </Link>
             </span>
             <span className="flex items-center gap-2 justify-center">
               <IoIosMail className="shrink-0" />
-              <Link href="mailto:info@dallasglassandauto.com" className="hover:underline hover:text-primary">
+              <Link
+                href="mailto:info@dallasglassandauto.com"
+                className="hover:underline hover:text-primary"
+              >
                 info@dallasglassandauto.com
               </Link>
             </span>
