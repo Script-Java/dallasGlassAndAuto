c39da71e591982bb25f289b264751caef698d0d7
Script-Java <atrindev@gmail.com>
2025-06-30 13:07:34 -0500

    update

--- src/app/components/howtocare.jsx
+++ src/app/components/howtocare.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function HowToCare() {
   return (
-    <div className="mt-12 flex flex-col items-center justify-center">
+    <div className="mt-12 flex flex-col p-4 items-center justify-center">
       <Link href="/blog/how-to" className="btn bg-primary p-8 md:p-4 text-white hover:bg-black">
         HOW TO CARE FOR A NEWLY REPLACED WINDSHIELD AND BACK GLASS
       </Link>
